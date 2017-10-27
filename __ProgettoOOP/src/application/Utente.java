03822a6d20731b685e739e33b48f8fcb991bdd76
clamar14 <marracciniclaudia@gmail.com>
2017-10-27 20:01:34 +0200

    .

--- __ProgettoOOP/src/application/Utente.java
+++ __ProgettoOOP/src/application/Utente.java
@@ -5,7 +5,6 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
